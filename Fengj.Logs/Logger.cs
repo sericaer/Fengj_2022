57ea3f69ec888b08e09fe4e1b7c254d9f287242f
sericaer <sericaer@outlook.com>
2022-05-06 11:40:23 +0800

    add logs

---
+++ Fengj.Logs/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fengj.Logs
+{
+    public static class Logger
+    {
+        private static Action<string> _info;
+        private static Action<string> _warin;
+        private static Action<string> _error;
+
+        public static void Init(Action<string> info, Action<string> warin, Action<string> error)
+        {
+            _info = info;
+            _error = error;
+            _warin = warin;
+        }
+
+        public static void Info(string str)
+        {
+            _info?.Invoke(str);
+        }
+
+        public static void Warn(string str)
+        {
+            _warin?.Invoke(str);
+        }
+
+        public static void Erro(string str)
+        {
+            _error?.Invoke(str);
+        }
+    }
+
+}
